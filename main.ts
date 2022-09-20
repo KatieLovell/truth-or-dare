47ada10a6cf4f32f9a1bd5bdae5e2122ff54e2e9
KatieLovell <112964812+KatieLovell@users.noreply.github.com>
2022-09-19 20:45:44 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,48 @@
-basic.forever(function () {
-	
+let t_or_d = 0
+let Qwerty = 0
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    t_or_d = randint(0, 1)
+    if (t_or_d == 0) {
+        basic.showString("dare")
+    } else if (t_or_d == 1) {
+        basic.showString("truth")
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    Qwerty = randint(0, 3)
+    if (0 == 0) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if ((0 as any) == (1 as any)) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else if ((0 as any) == (2 as any)) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if ((0 as any) == (3 as any)) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
 })
